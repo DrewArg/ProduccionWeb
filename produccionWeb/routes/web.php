9e4cd7fcbfcff71812235fbbd1755d669eecb09a
PabloBPafundi <128729717+PabloBPafundi@users.noreply.github.com>
2023-06-04 18:07:39 -0300

    update layout

--- produccionWeb/routes/web.php
+++ produccionWeb/routes/web.php
@@ -13,8 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+/*
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+*/
+
+Route::get('/', function () {
+    return view('index');
 });
 
 Auth::routes();
