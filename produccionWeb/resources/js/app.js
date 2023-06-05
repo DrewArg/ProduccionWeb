2fabc253d5b11788b3dc7bd456927a3e0e0cc838
PabloBPafundi <128729717+PabloBPafundi@users.noreply.github.com>
2023-06-05 13:59:02 -0300

    update layout

--- produccionWeb/resources/js/app.js
+++ produccionWeb/resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+
+// Popover 
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
--- produccionWeb/resources/views/layouts/layout.blade.php
+++ produccionWeb/resources/views/layouts/layout.blade.php
@@ -1,24 +1,14 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-{{--       <link rel="stylesheet" href="/produccionWeb/old/style.css">--}}
-{{--    <link rel="stylesheet" href="/produccionWeb/old/index.css">--}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;900&display=swap" rel="stylesheet">
-
-    <title>Libreria</title>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'BookZone') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -30,7 +20,8 @@
 </head>
 
 <body>
-    <header class="nav__global">
+    
+    <!-- <header class="nav__global">
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container-fluid">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -70,18 +61,224 @@
             </div>
         </nav>
 
-    </header>
+    </header> -->
 
 
-<main class="py-4">
+<header class="mb-5 fixed-top nav__global">
+
+<div class="border-bottom">
+  <div class="container d-flex justify-content-between align-items-center py-1">
+    
+      <h1 class="fs-6 align-self-center my-0">Buenos Aires | WhatsApp 011-15-11111111 | Mail ejemplo@ejemplo.com</h1>
+      
+    <div class="d-flex"> 
+    <i class="fa-solid fa-globe align-self-center fa-lg me-2"></i>
+      <select class="form-select" aria-label="Default select example">
+        <option selected value="1">Español</option>
+        <option value="2">English</option>
+      </select>
+    </div>
+  </div>
+</div>
+
+
+<nav class="navbar navbar-expand-lg ">
+  <div class="container">
+
+    <a class="navbar-brand fs-1" href="./index.php">Book Zone</a>
+   
+
+
+    <div class="">
+
+    <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Buscar producto..." aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="./index.php">Inicio</a>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Productos
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="../pages/productos.php">Todos los productos</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        
+
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="./quienes_somos.php">FAQ</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="./quienes_somos.php">Contacto</a>
+        </li>
+      
+    
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="./quienes_somos.php">Quienes somos?</a>
+        </li>
+
+      </ul>
+     
+    </div>
+
+
+    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button> -->
+
+    </div>
+
+
+<div>
+  <p class="">
+    <a class="nav-link d-inline" href="{{ route('register') }}">{{ __('Crear cuenta') }}</a>
+    <span>|</span>
+    <a class="nav-link d-inline" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
+  </p>
+
+
+  <div class="d-flex justify-content-center">
+
+  <a href="../pages/carrito.php" class="nav-link"><i class="fa-solid fa-cart-plus fa-2x"></i></a>
+  
+  </div>
+</div>
+
+  </div>
+</nav>
+
+</header>
+
+
+<main class="margin__top container">
             @yield('content')
-        </main>
+</main>
 
 
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+
+
+
+
+
+<button class="btn whatsapp-icon" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-content="Contactanos por WhatsApp">
+  <a class="nav-link" target="_blank" href="https://wa.me/5491111111111"><i class="fa-brands fa-square-whatsapp fa-4x"></i></a>
+</button>
+
+
+
+<footer class="py-3 mt-4 sticky-footer nav__global">
+
+<div class="container">
+<div class="row d-flex justify-content-between">
+
+<div class="col-5 row">
+    
+
+
+<div class="col-5">
+
+
+<h4>Contacto</h4>
+<hr>
+<p>
+    Whatsapp: <br>
+    011-15-11111111
+</p>
+
+<p>
+    Mail: <br>
+    mail@café.com
+</p>
+
+
+
+</div>
+
+
+
+<div class="col-5 text-start">
+
+
+<h4>Navegación</h4>
+<hr>
+
+<ul class="p-0">
+
+<li>Inicio</li>
+<li>Productos</li>
+<li>Inicio</li>
+<li>Contacto</li>
+<li>Quines Somos?</li>
+<li>FAQ</li>
+</ul>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+<div class="col-3 align-self-end">
+
+
+
+
+<h4>Redes sociales</h4>
+<hr>
+
+
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+</footer>
+
+
+
+
+<!-- Font Awesome  -->
+<script src="https://kit.fontawesome.com/60be70fbe4.js" crossorigin="anonymous"></script>
+
 </body>
-
 </html>
