f42bba7499161cfbd778d7f057f525f6a1553dd9
PabloBPafundi <128729717+PabloBPafundi@users.noreply.github.com>
2023-08-06 16:36:35 -0300

    crudPreguntas

--- produccionWeb/resources/views/common/contacto/index.blade.php
+++ produccionWeb/resources/views/common/contacto/index.blade.php
@@ -20,65 +20,56 @@
 
 
 
-    <form class="row g-4">
+ <form method="POST" action="{{ route('contacto.procesar') }}" class="row g-4">
+    @csrf
 
+    <div class="col-md-6 mb-3">
+        <label for="formGroupExampleInput" class="form-label">Nombre</label>
+        <input type="text" class="form-control @error('nombre') is-invalid @enderror" id="formGroupExampleInput"
+            placeholder="Nombre" name="nombre">
 
+        @error('nombre')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
 
+    <div class="col-md-6 mb-3">
+        <label for="formGroupExampleInput" class="form-label">Email</label>
+        <input type="email" class="form-control @error('email') is-invalid @enderror" id="formGroupExampleInput"
+            placeholder="ejemplo@ejemplo.com" name="email">
 
-        <div class="col-md-4 mb-3">
+        @error('email')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
 
-            <label for="formGroupExampleInput" class="form-label">Nombre</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nombre">
+    <div class="col-md-12">
+        <label for="exampleFormControlTextarea1" class="form-label fs-4">En qué podemos ayudarte?</label>
+        <textarea class="form-control @error('preguntas') is-invalid @enderror" id="exampleFormControlTextarea1"
+            rows="3" name="preguntas"></textarea>
 
+        @error('preguntas')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
 
-
-        </div>
-
-
-        <div class="col-md-4 mb-3">
-
-            <label for="formGroupExampleInput" class="form-label">Apellido</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Apellido">
-
-
-
-        </div>
-
-
-
-        <div class="col-md-4 mb-3">
-
-            <label for="formGroupExampleInput" class="form-label">Email</label>
-            <input type="email" class="form-control" id="formGroupExampleInput" placeholder="ejemplo@ejemplo.com">
-
-
-
-        </div>
-
-
-
-
-
-
-        <div class="col-md-12">
-
-            <label for="exampleFormControlTextarea1" class="form-label fs-4">En que podemos ayudarte?</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-
-
-        </div>
-
-
-
-
-
-        <div class="col-12 text-center">
-            <button class="btn btn-primary" type="submit">Enviar consulta</button>
-          </div>
-
-
+    <div class="col-12 text-center mb-1">
+        <button class="btn btn-primary" type="submit">Enviar consulta</button>
+    </div>
 </form>
 
-</div>
 
+@if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+</div>
 @endsection
--- produccionWeb/routes/web.php
+++ produccionWeb/routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\PreguntaController;
 use App\Http\Controllers\RevisionController;
 
 
+
 Route::get('/',[
     ProductoController::class,'home_destacados'
 ])->name('index');
@@ -35,5 +36,6 @@ Route::resource('/admin/usuarios', UsuarioController::class);
 Route::get('/admin/productos',[ProductoController::class,'admin_index'])->name('productos.admin_index');
 Route::resource('/admin/preguntas', PreguntaController::class);
 Route::resource('/admin/revisiones', RevisionController::class);
+Route::post('/contacto', [PreguntaController::class, 'procesarFormulario'])->name('contacto.procesar');
 
 Auth::routes();
