ba1f52531293d7ec3de99bd9adc20e64bbcfba49
DrewArg <andresfabbiano5@gmail.com>
2023-08-06 18:03:24 -0300

    borra footer y whatsapp cion

--- produccionWeb/app/Http/Controllers/Auth/LoginController.php
+++ produccionWeb/app/Http/Controllers/Auth/LoginController.php
@@ -8,16 +8,6 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
     use AuthenticatesUsers;
 
@@ -36,5 +26,24 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Validate the user's credentials.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function validateCredentials($request, $credentials)
+    {
+        $plainPassword = $credentials['password'];
+
+        // Deshasheamos la contraseña almacenada en la base de datos usando Auth::getProvider()->getHasher()
+        $hashedPassword = Auth::getProvider()->getHasher()->getHash();
+
+        return [
+            'email' => $credentials['email'],
+            'password' => $plainPassword,
+        ];
     }
 }
